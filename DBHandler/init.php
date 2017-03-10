a0e9ca802bc85e89d776f188972be401188c0805
Mohammad Julfikar Mahmud <jxmxshakil@gmail.com>
2017-03-10 18:53:39 +0800

    fb SDK

---
+++ DBHandler/init.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mohammad
+ * Date: 10/03/17
+ * Time: 6:39 PM  cdf3dfede3507396345277a82a1acd80
+ */
+require_once __DIR__.'../vendor/autoload.php';
+
+$fb = new \Facebook\Facebook([
+   'app_id'=>'{147468312440100}',
+    'app_secret' => '{cdf3dfede3507396345277a82a1acd80}',
+    'default_graph_version' => 'v2.5',
+]);
